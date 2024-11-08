b0b4832b50439731c3398ee0e133ef15d9430d61
zongzhili <zongzhil@nycstudents.net>
2024-11-08 13:57:35 -0500

    aa

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,13 @@ import "/styles/style.css";
 import { foodProducts } from "./foods.js";
 
 document.querySelector(".btn").addEventListener("click", function () {
-  document.body.classList.add("hot");
-
-  /*  if (document.body.classList.contains("cold")) {
+  if (document.body.classList.contains("cold")) {
     document.body.classList.add("hot");
     document.body.classList.remove("cold");
-  }*/
+  } else {
+    document.body.classList.add("cold");
+    document.body.classList.remove("hot");
+  }
 });
 
 /*function removeCards(cards) {
